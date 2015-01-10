d3531026706737aa18e502ab55abc0fb1daacc94
Julien Guanzon <julienguanzon@gmail.com>
2015-01-10 16:05:19 -0700

    Add Hello World

--- AGoodStart-Swift/AGoodStart-Swift/AppDelegate.swift
+++ AGoodStart-Swift/AGoodStart-Swift/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
 
         // Insert code here...
-        
+        print("Hello World");
         
         
         return true
